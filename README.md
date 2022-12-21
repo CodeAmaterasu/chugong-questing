918047421d1b31cb72e849265a56f2580e46b0ff
Danilo Jakob <danilo.murer@gmail.com>
2022-12-21 19:01:16 +0100

    CHANGE: Created TypeORM connection to local mongodb. CHANGE: Added docker-compose for local mongodb database

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 Backend is developed using NestJs (/server) and the frontend using Vue.js (/client).
 
 DevOps will be implemented using Docker, Jenkins and Kubernetes.
+
+## Run MongoDB
+
+Simply run the 'MongoDB' WebStorm Run Configuration.
+
+If not possible, the database can be run with following command:
+```bash
+cd .docker/
+docker-compose up -d
+```
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './data/User';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      username: 'root',
+      password: 'root',
+      database: 'chugong',
+      entities: [User],
+      synchronize: true,
+      authSource: 'admin',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
